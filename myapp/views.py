b3c0e1456061a68a02a5846f82d6bd9d2d1156ff
Madhup Shrivastava <manushrivastava528@gmail.com>
2021-11-05 21:04:42 +0530

    print added

--- myapp/views.py
+++ myapp/views.py
@@ -4,11 +4,11 @@ from rest_framework.response import Response
 # Create your views here.
 class Webhook(APIView):
     def post(self, request):
-        print(request.data)
+        print("\n\n\n************************************",request.data,"\n\n\n\n**********************************")
 
         return Response(data="Sucess")
     
     def get(self,request):
-        print(request.GET)
+        print("\n\n\n************************************",request.GET,"\n\n\n\n**********************************")
 
         return Response(data="Sucess")
